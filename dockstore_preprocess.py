cb6c7b3d38cc1ddefac355abc3bbbaad85a76e86
f73chen <grace.fenglin@gmail.com>
2020-07-14 18:45:06 -0400

    Update dockstore_preprocess.py

--- dockstore_preprocess.py
+++ dockstore_preprocess.py
@@ -14,7 +14,7 @@ doc = WDL.load(args.input_wdl_path)         # loads the entire document
 def tabs_to_spaces(num_spaces):     # what about multiple tabs, or tab is in a string?
     for line in doc.source_lines:
         while re.search(r'(?<=\n)( *)\t', line):
-        line = re.sub(r'(?<=\n)( *)\t', '\g<1>        ', line)
+            line = re.sub(r'(?<=\n)( *)\t', '\g<1>        ', line)
 
 # add docker to every task and workflow explicitly
 def docker_runtime():

1b128288f8032985e92e9a74695cc858c878f991
f73chen <grace.fenglin@gmail.com>
2020-07-14 11:45:15 -0400

    Update dockstore_preprocess.py

--- dockstore_preprocess.py
+++ dockstore_preprocess.py
@@ -3,9 +3,15 @@
 import argparse
 import WDL
 
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--input-wdl-path", required=True)
+args = parser.parse_args()
+
+doc = WDL.load(args.input_wdl_path)     # loads the entire document
+
 # find all params that need to be replaced, for example:
 def test():
-    for input in doc.workflow.inputs:     # for each line in the workflow inputs
+    for input in doc.workflow.inputs:       # for each line in the workflow inputs
         index = doc.source_lines[input.pos.line - 1].find("File? chimeric")   # now it detects the number of spaces in front
         if index > -1:      # if that line is found
             print(index)
@@ -14,18 +20,11 @@ def test():
 
 # final outputs to stdout or a file with modified name
 def write_out():
-    #print("\n".join(w.source_lines))
+    # print("\n".join(w.source_lines))      # prints the entire workflow to stdout
 
     name_index = args.input_wdl_path.rfind('/')
-    print(name_index)
     output_path = args.input_wdl_path[:name_index + 1] + "dockstore_" + args.input_wdl_path[name_index + 1:]
-    print(output_path)
-    # with open()
-
-parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("--input-wdl-path", required=True)
-args = parser.parse_args()
-
-doc = WDL.load(args.input_wdl_path)     # loads the entire document
-#test()
+    with open(output_path, "w") as output_file:
+        output_file.write("\n".join(w.source_lines))
+test()
 write_out()

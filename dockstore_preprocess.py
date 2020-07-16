335763eebfdcd0b991a542097bdb78efd9c8dafa
f73chen <grace.fenglin@gmail.com>
2020-07-16 12:17:43 -0400

    Update dockstore_preprocess.py

--- dockstore_preprocess.py
+++ dockstore_preprocess.py
@@ -36,10 +36,10 @@ def find_indices(line, target):
     return index1, index2
 
 # helper function: add "docker = docker" to a call with a multi-line input section
-def docker_to_call_inputs_multiline(part):
+def docker_to_call_inputs_multiline(call):
     # either multi-line has docker or hasn't, but will not be empty (that's single line)
-    line_pos = part.pos.line - 1
-    if "docker" not in part.inputs.keys():  # add docker as new var
+    line_pos = call.pos.line - 1
+    if "docker" not in call.inputs.keys():  # add docker as new var
         line_pos += 2 if "input:" in doc.source_lines[line_pos + 1] else 1   # first line with input vars
         line = doc.source_lines[line_pos]
         num_spaces = len(line) - len(line.lstrip(' '))
@@ -55,13 +55,13 @@ def docker_to_call_inputs_multiline(part):
         doc.source_lines[line_pos] = line
 
 # helper function: add "docker = docker" to a call with a single line input section
-def docker_to_call_inputs_single_line(part):
-    line = doc.source_lines[part.pos.line - 1]
-    if not part.inputs:     # if input section empty, add "input: docker"
+def docker_to_call_inputs_single_line(call):
+    line = doc.source_lines[call.pos.line - 1]
+    if not call.inputs:     # if input section empty, add "input: docker"
         index = len(line) - 1                   # if call doesn't have {}, set index to end of line
         line += " { input: docker = docker }"
 
-    elif "docker" not in part.inputs.keys():    # if input not empty but no docker var: add it
+    elif "docker" not in call.inputs.keys():    # if input not empty but no docker var: add it
         index = line.rfind('}')
         index -= (line[index - 1] == ' ')       # move one back if " }"
         line = line[:index] + ", docker = docker" + line[index:]
@@ -70,21 +70,21 @@ def docker_to_call_inputs_single_line(part):
         index1, index2 = find_indices(line = line, target = "docker")
         line = line[:index1] + "docker" + line[index2:]
 
-    doc.source_lines[part.pos.line - 1] = line
+    doc.source_lines[call.pos.line - 1] = line
 
 # add String docker to workflow inputs
-def docker_to_workflow_inputs(num_spaces = 4):
-    if not doc.workflow.inputs:
-        line = doc.source_lines[doc.workflow.pos.line - 1]
+def docker_to_workflow_or_task_inputs(num_spaces = 4, body):    # where body is a workflow or task
+    if not body.inputs:
+        line = doc.source_lines[body.pos.line - 1]
         line += '\n' + \
                 ' ' * num_spaces + 'input {\n' + \
                 ' ' * num_spaces * 2 + 'String docker = "' + args.docker_image + '"\n' + \
                 ' ' * num_spaces + '}\n'
-        doc.source_lines[doc.workflow.pos.line - 1] = line
+        doc.source_lines[body.pos.line - 1] = line
 
     else:   # if inputs section does exist
         docker_in_inputs = False
-        for input in doc.workflow.inputs:   # replace existing docker var
+        for input in body.inputs:   # replace existing docker var
             if "docker" in input.name:
                 docker_in_inputs = True
                 line = doc.source_lines[input.pos.line - 1]
@@ -93,10 +93,10 @@ def docker_to_workflow_inputs(num_spaces = 4):
                 doc.source_lines[input.pos.line - 1] = line
 
         if not docker_in_inputs:            # add new docker var
-            line = doc.source_lines[doc.workflow.inputs[0].pos.line - 1]
+            line = doc.source_lines[body.inputs[0].pos.line - 1]
             num_spaces = len(line) - len(line.lstrip(' '))
             line = ' ' * num_spaces + 'String docker = "' + args.docker_image + '"\n' + line
-            doc.source_lines[doc.workflow.inputs[0].pos.line - 1] = line
+            doc.source_lines[body.inputs[0].pos.line - 1] = line
 
 # find all nested calls within a workflow
 def find_calls():
@@ -128,7 +128,7 @@ def docker_runtime():
         return
 
     # add image to workflow inputs
-    docker_to_workflow_inputs(num_spaces = 4)
+    docker_to_workflow_or_task_inputs(body = doc.workflow)
 
     # add image to all task calls
     call_list = find_calls()
@@ -162,8 +162,10 @@ def source_modules():
                 doc.source_lines[pos] = prepend + doc.source_lines[pos][num_spaces:]
 
 # TEST FUNCTION
-def test(num_spaces = 4):
-    print("test")
+def test():
+    # add image to all tasks
+    for task in doc.tasks:
+        docker_to_workflow_or_task_inputs(body = task)
 
 # final outputs to stdout or a file with modified name
 def write_out():
@@ -174,12 +176,12 @@ def write_out():
 
 tabs_to_spaces()                                # tested - convert tabs to spaces
 # find_indices(line, target)                    # tested - isolate start and end of target's expression, special if string
-docker_runtime()
+# docker_runtime()
     # find_calls()                              # tested - find all nested calls in a workflow
-    # docker_to_call_inputs_multiline(part)     # tested - add or convert docker for multi-line call
-    # docker_to_call_inputs_single_line(part)   # tested - add or convert docker for single-line call
-    # docker_to_workflow_inputs(num_spaces = 4) # tested - add or convert docker for workflow inputs
+    # docker_to_call_inputs_multiline(call)     # tested - add or convert docker for multi-line call
+    # docker_to_call_inputs_single_line(call)   # tested - add or convert docker for single-line call
+    # docker_to_workflow_or_task_inputs(num_spaces = 4) # tested - add or convert docker for workflow inputs
 # pull_to_root()
 # source_modules()                              # tested - add source; module if "modules" var exists, else don't
-# test()
+test()
 write_out()                                     # tested - write out

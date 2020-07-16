ce1d4e19fda8311b970486547430dac78b6265b7
f73chen <grace.fenglin@gmail.com>
2020-07-15 21:10:46 -0400

    Update dockstore_preprocess.py

--- dockstore_preprocess.py
+++ dockstore_preprocess.py
@@ -114,7 +114,7 @@ def source_modules():
 # find all params that need to be replaced
 def test():
     if not doc.workflow.inputs:
-        print("workflow has no inputs section")
+        print(doc.source_lines[doc.workflow.inputs.pos.line - 1])
     else:
         if "docker" not in doc.workflow.inputs:
             print("append inputs with docker")

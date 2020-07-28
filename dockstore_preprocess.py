8cf4eb7a2611e575b3dbdbe73fde6085d8a7e8c4
f73chen <grace.fenglin@gmail.com>
2020-07-28 12:40:08 -0400

    Update dockstore_preprocess.py

--- dockstore_preprocess.py
+++ dockstore_preprocess.py
@@ -408,6 +408,7 @@ def test():
     var_parameter_meta(body = doc.workflow, target = "task2_var1", description = "new description for task2_var1")  # add new line
     var_parameter_meta(body = doc.tasks[1], target = "var1", description = "new meta section + var1 description")   # add new section
     var_parameter_meta(body = doc.tasks[1], target = "var1", description = "replacement var1 description")          # replace description
+    var_parameter_meta(body = doc.tasks[1], target = "var2", description = "add new var2 before var1")              # replace description
 
 tabs_to_spaces()                            # convert tabs to spaces
 #pull_to_root()                              # pull json-specified task variables to the workflow that calls them

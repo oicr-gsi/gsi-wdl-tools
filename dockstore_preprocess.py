ef1ea5e7e478d0ffdc211e2468938f64b8ec9d55
f73chen <grace.fenglin@gmail.com>
2020-07-28 11:33:05 -0400

    Update dockstore_preprocess.py

--- dockstore_preprocess.py
+++ dockstore_preprocess.py
@@ -416,12 +416,10 @@ def test():
         if indicator in line and (line.find('#') < 0 or line.find(indicator) < line.find('#')):
             break       # stop searching
         pos += 1        # if not found, increase index
+    while doc.source_lines[pos].find("parameter_meta") < 0:
+        pos+= 1
     for i in range(5):
         print(doc.source_lines[pos + i])
-    # while "parameter_meta" not in doc.source_lines[pos]:
-    #     pos+= 1
-    # for i in range(5):
-    #     print(doc.source_lines[pos + i])
 
 
     # then keep going down until found parameter_meta section

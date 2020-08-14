435ff4aba08debca7adbf1f444013c4d6db7b24c
f73chen <grace.fenglin@gmail.com>
2020-08-14 18:07:09 -0400

    assert some input args

--- tests/test_dockstore_preprocess.py
+++ tests/test_dockstore_preprocess.py
@@ -1,16 +1,30 @@
-import subprocess as sb
+import scripts.dockstore_preprocess as dp
+#import filecmp
 
-# def test_dockstore_preprocess(shared_datadir):
-#     workflow_path = (shared_datadir / 'workflow1.wdl').as_posix()
-#     #sb.Popen(['ls',  '-l', workflow_path])
-#     sb.Popen(['cd', 'scripts/'])
-#     sb.Popen(['pwd'])
-#     sb.Popen(['python3', 'dockstore_preprocess.py', '--input-wdl-path', workflow_path, '--pull-all', 'true', '--dockstore', 'true'])
-#     assert 0
+def test_dockstore_preprocess(shared_datadir):
+    workflow_path = (shared_datadir / 'workflow1.wdl').as_posix()
+    #pull_path = (shared_datadir / 'pull_workflow1.wdl').as_posix()
+    #dockstore_path = (shared_datadir / 'dockstore_workflow1.wdl').as_posix()
 
-from diagnose import *
+    args = ['--input-wdl-path', str(workflow_path),
+            '--tab-size', '4'
+            '--pull-all', 'True',
+            '--dockstore', 'True',
+            '--docker_image', 'g3chen/wgsPipeline:2.0',
+            '--import_metas', 'False',
+            '--output-wdl-path', str(shared_datadir / 'dockstore_workflow1.wdl')]
+    parsed = vars(dp.parse_inputs(args))
 
-def test_diagnose(shared_datadir):
-    args = main(['--input-wdl-path', str(shared_datadir) + '/workflow1.wdl'])
-    print(args)
-    assert 0
+    assert parsed['input_wdl_path'] == str(workflow_path)
+    assert parsed['tab_size'] == '4'
+    assert parsed['pull_all'] == 'True'
+    assert parsed['dockstore'] == 'True'
+    assert parsed['docker_image'] == 'g3chen/wgsPipeline:2.0'
+    assert parsed['import_metas'] == 'False'
+    assert parsed['output_wdl_path'] == str(shared_datadir / 'dockstore_workflow1.wdl')
+
+    # preprocess workflow1 and put in same dir
+
+    # compare content between fresh and old dockstore_.wdl
+    #assert filecmp.cmp(@@@PULLED FILE, pull_path)
+    #assert filecmp.cmp(@@@DOCKSTORE FILE, dockstore_path)

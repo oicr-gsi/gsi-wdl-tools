fb8eeda30112c03b00dd2412beec87eb8f9f999a
f73chen <grace.fenglin@gmail.com>
2020-08-14 14:18:16 -0400

    Update test_dockstore_preprocess.py

--- tests/test_dockstore_preprocess.py
+++ tests/test_dockstore_preprocess.py
@@ -1,5 +1,8 @@
 import os
 
 def test_dockstore_preprocess(shared_datadir):
+    print(shared_datadir)
     os.system("ls -l data/workflow1.wdl")
     #eval "python3 $preprocess --input-wdl-path $file $args";
+
+test_dockstore_preprocess

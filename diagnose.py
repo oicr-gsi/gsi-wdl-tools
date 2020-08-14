bde67671672832bb5e85e9226914a2be71110be2
f73chen <grace.fenglin@gmail.com>
2020-08-14 15:49:15 -0400

    Create diagnose.py

---
+++ diagnose.py
@@ -0,0 +1,23 @@
+import argparse
+import WDL
+import sys
+
+def main(args):
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-i", "--input-wdl-path", required = True, help = "source wdl path")
+    parser.add_argument("-d", "--docker-image", required = False, help = "image name and tag")
+    parser.add_argument("-j", "--pull-json", required = False, help = "path to json containing which variables to pull; don't specify --pull-all at the same time")
+    parser.add_argument("-o", "--output-wdl-path", required = False, help = "output wdl path")
+    parser.add_argument("-t", "--tab-size", required = False, help = "number of spaces in a tab")
+    parser.add_argument("-p", "--pull-all", required = False, type=bool, help = "whether to pull all variables; don't specify --pull-json at the same time")
+    parser.add_argument("-s", "--dockstore", required = False, type=bool, help = "whether to activate functions for dockstore")
+    parser.add_argument("-w", "--import-metas", required = False, type=bool, help = "whether to pull parameter_metas from imported subworkflows")
+    caller(**vars(parser.parse_args(args)))
+
+def caller(input_wdl_path = None, docker_image = None, pull_json = None, output_wdl_path = None, tab_size = None, pull_all = None, dockstore = None, import_metas = None):
+    doc = WDL.load(input_wdl_path)  # loads the file as a WDL.Tree.Document object
+    print(doc.workflow.name)
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
+

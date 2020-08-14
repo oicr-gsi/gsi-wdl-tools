4b9377cdff8834e1eaf01132191def3208dbaacc
f73chen <grace.fenglin@gmail.com>
2020-08-14 15:15:57 -0400

    Update test_dockstore_preprocess.py

--- tests/test_dockstore_preprocess.py
+++ tests/test_dockstore_preprocess.py
@@ -3,5 +3,7 @@ import subprocess as sb
 def test_dockstore_preprocess(shared_datadir):
     workflow_path = (shared_datadir / 'workflow1.wdl').as_posix()
     #sb.Popen(['ls',  '-l', workflow_path])
-    sb.Popen(['python3', '$PWD/scripts/dockstore_preprocess.py', '--input-wdl-path', workflow_path, '--pull-all', 'true', '--dockstore', 'true'])
+    sb.Popen(['cd', 'scripts/'])
+    sb.Popen(['pwd'])
+    sb.Popen(['python3', 'dockstore_preprocess.py', '--input-wdl-path', workflow_path, '--pull-all', 'true', '--dockstore', 'true'])
     assert 0

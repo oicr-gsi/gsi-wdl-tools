fc394d74579561abb637ad1a0d12f76dbd99446b
f73chen <grace.fenglin@gmail.com>
2020-08-14 15:52:31 -0400

    Update test_dockstore_preprocess.py

--- tests/test_dockstore_preprocess.py
+++ tests/test_dockstore_preprocess.py
@@ -1,9 +1,15 @@
 import subprocess as sb
 
-def test_dockstore_preprocess(shared_datadir):
-    workflow_path = (shared_datadir / 'workflow1.wdl').as_posix()
-    #sb.Popen(['ls',  '-l', workflow_path])
-    sb.Popen(['cd', 'scripts/'])
-    sb.Popen(['pwd'])
-    sb.Popen(['python3', 'dockstore_preprocess.py', '--input-wdl-path', workflow_path, '--pull-all', 'true', '--dockstore', 'true'])
+# def test_dockstore_preprocess(shared_datadir):
+#     workflow_path = (shared_datadir / 'workflow1.wdl').as_posix()
+#     #sb.Popen(['ls',  '-l', workflow_path])
+#     sb.Popen(['cd', 'scripts/'])
+#     sb.Popen(['pwd'])
+#     sb.Popen(['python3', 'dockstore_preprocess.py', '--input-wdl-path', workflow_path, '--pull-all', 'true', '--dockstore', 'true'])
+#     assert 0
+
+from diagnose import *
+
+def test_diagnose(shared_datadir):
+    print(import successful)
     assert 0

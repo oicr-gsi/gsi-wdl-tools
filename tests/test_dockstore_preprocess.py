8ed819e14ce0f2c30e07f77037521c1a21dc7699
f73chen <grace.fenglin@gmail.com>
2020-08-18 11:45:00 -0400

    Update test_dockstore_preprocess.py

--- tests/test_dockstore_preprocess.py
+++ tests/test_dockstore_preprocess.py
@@ -9,7 +9,7 @@ def parse_inputs(args):
     parser.add_argument("-d", "--docker-image", required = False, help = "image name and tag")
     parser.add_argument("-j", "--pull-json", required = False, help = "path to json containing which variables to pull; don't specify --pull-all at the same time")
     parser.add_argument("-o", "--output-wdl-path", required = False, help = "output wdl path")
-    parser.add_argument("-t", "--tab-size", required = False, type=int, help = "number of spaces in a tab")
+    parser.add_argument("-t", "--tab-size", required = False, help = "number of spaces in a tab")
     parser.add_argument("-p", "--pull-all", required = False, type=bool, help = "whether to pull all variables; don't specify --pull-json at the same time")
     parser.add_argument("-s", "--dockstore", required = False, type=bool, help = "whether to activate functions for dockstore")
     parser.add_argument("-w", "--import-metas", required = False, type=bool, help = "whether to pull parameter_metas from imported subworkflows")

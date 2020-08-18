87adc18fc9b6bc7ca7fd952e9689b5631ef907d9
f73chen <grace.fenglin@gmail.com>
2020-08-18 11:30:41 -0400

    Update test_dockstore_preprocess.py

--- tests/test_dockstore_preprocess.py
+++ tests/test_dockstore_preprocess.py
@@ -10,9 +10,10 @@ def parse_inputs(args):
     parser.add_argument("-j", "--pull-json", required = False, help = "path to json containing which variables to pull; don't specify --pull-all at the same time")
     parser.add_argument("-o", "--output-wdl-path", required = False, help = "output wdl path")
     parser.add_argument("-t", "--tab-size", required = False, help = "number of spaces in a tab")
-    parser.add_argument("-p", "--pull-all", required = False, type=bool, help = "whether to pull all variables; don't specify --pull-json at the same time")
-    parser.add_argument("-s", "--dockstore", required = False, type=bool, help = "whether to activate functions for dockstore")
-    parser.add_argument("-w", "--import-metas", required = False, type=bool, help = "whether to pull parameter_metas from imported subworkflows")
+    parser.add_argument("-p", "--pull-all", required = False, action="store_true", type=bool, help = "whether to pull all variables; don't specify --pull-json at the same time")
+    parser.add_argument("-s", "--dockstore", required = False, action="store_true", type=bool, help = "whether to activate functions for dockstore")
+    parser.add_argument("-w", "--import-metas", required = False, action="store_true", type=bool, help = "whether to pull parameter_metas from imported subworkflows")
+    print(type(parser.parse_args(args)))
     return parser.parse_args(args)
 
 def test_dockstore_preprocess(shared_datadir):
@@ -25,28 +26,25 @@ def test_dockstore_preprocess(shared_datadir):
 
     args = ['--input-wdl-path', str(workflow_path),
             '--tab-size', '4'
-            '--pull-all', 'true',
-            '--dockstore', 'true',
+            '--pull-all',
+            '--dockstore',
             '--docker-image', '"g3chen/wgsPipeline:2.0"',
-            '--import-metas', 'false',
             '--output-wdl-path', str(dockstore_path)]
     print("-----------direct parsing---------")
     print(parse_inputs(args))
     print("-----------passed to dockstore_preprocess--------------")
     print(dp.parse_inputs(args))    # should be the same as ^
     # print("-----------assignment-------------------")
-    # parsed = vars(dp.parse_inputs(args))
-    #
-    # assert parsed['input_wdl_path'] == str(workflow_path)
-    # assert parsed['tab_size'] == '4'
+    # assert parsed.input_wdl_path == str(workflow_path)
+    # assert parsed.tab_size == '4'
     # print("here1")
-    # assert parsed['pull_all'] == 'True'
+    # assert parsed.pull_all == 'True'
     # print("here2")
-    # assert parsed['dockstore'] == 'True'
+    # assert parsed.dockstore == 'True'
     # print("here3")
-    # assert parsed['docker_image'] == 'g3chen/wgsPipeline:2.0'
-    # assert parsed['import_metas'] == 'False'
-    # assert parsed['output_wdl_path'] == str(dockstore_path)
+    # assert parsed.docker_image == 'g3chen/wgsPipeline:2.0'
+    # assert parsed.import_metas == 'False'
+    # assert parsed.output_wdl_path == str(dockstore_path)
 
     # preprocess workflow1 and put in same dir
 

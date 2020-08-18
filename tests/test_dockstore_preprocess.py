0130aa9a938340d16c6425df01b0cfea1cdee624
f73chen <grace.fenglin@gmail.com>
2020-08-18 11:39:00 -0400

    Update test_dockstore_preprocess.py

--- tests/test_dockstore_preprocess.py
+++ tests/test_dockstore_preprocess.py
@@ -5,7 +5,7 @@ import sys
 
 def parse_inputs(args):
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("-i", "--input-wdl-path", required = True, help = "source wdl path")
+    parser.add_argument("-i", "--input-wdl-path", required = False, help = "source wdl path")
     parser.add_argument("-d", "--docker-image", required = False, help = "image name and tag")
     parser.add_argument("-j", "--pull-json", required = False, help = "path to json containing which variables to pull; don't specify --pull-all at the same time")
     parser.add_argument("-o", "--output-wdl-path", required = False, help = "output wdl path")

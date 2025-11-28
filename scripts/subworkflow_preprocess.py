da7c2e2c7cd1a480c85a2f5f3eb3154f1af3adc1
Savo Lazic <slazic@oicr.on.ca>
2025-11-28 15:26:34 -0500

    Support for dictionaries in parameter_meta
    This is used for vidarr_retry syntax

--- scripts/subworkflow_preprocess.py
+++ scripts/subworkflow_preprocess.py
@@ -20,6 +20,13 @@ has_param_meta = []     # names of tasks or workflow that have a parameter_meta 
 doc = None              # WDL document object
 tab_size = 4
 
+# WDL keeps its own boolean values, that cannot be dealt with when in comes to JSON serialization
+class WDLEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, WDL.Expr.Boolean):
+            return bool(o)
+        return super().default(o)
+
 # lv. 1 --> only calls default methods; can be used anywhere
 # lv. 2 --> calls lv. 1 functions; placed below lv. 1
 
@@ -409,7 +416,10 @@ def pull_to_root_all():
         # pull parameter_metas
         body = call.callee      # the task or workflow that the call refers to
         if str(input.name) in body.parameter_meta.keys():   # if the original variable had a meta description
-            old_description = '"' + body.parameter_meta[str(input.name)] + '"'
+            if isinstance(body.parameter_meta[str(input.name)], dict):
+                old_description = json.dumps(body.parameter_meta[str(input.name)], cls=WDLEncoder)
+            else:
+                old_description = '"' + body.parameter_meta[str(input.name)] + '"'
             var_parameter_meta(body=doc.workflow, target=extended_name, description=old_description)  # pull description to root
         # know that input is not in the call inputs already (else wouldn't be part of available_inputs)
         line = doc.source_lines[call.pos.line - 1]
